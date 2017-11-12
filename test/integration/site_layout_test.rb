d45010f685940ae830fa91d064a05f2ea1240c92
Josh_Gerth <jgerth@ggc.edu>
2017-11-12 07:51:22 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,13 +7,29 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
       assert_template 'static_pages/home'
       assert_select "a[href=?]", root_path, count: 2
       assert_select "a[href=?]", help_path
-      get help_path
-      assert_select "title", full_title("Help")
+        get help_path
+        assert_select "title", full_title("Help")
       assert_select "a[href=?]", about_path
-      get about_path
-      assert_select "title", full_title("About")
+        get about_path
+        assert_select "title", full_title("About")
       assert_select "a[href=?]", contact_path
-      get contact_path
-      assert_select "title", full_title("Contact")
+        get contact_path
+        assert_select "title", full_title("Contact")
+      assert_select "a[href=?]", login_path
+        get login_path
+        assert_select "title", full_title("Log in")
+      user = users(:michael)
+      log_in_as(user)
+      get root_path
+        assert_select "a[href=?]", logout_path
+        assert_select "a[href=?]", users_path
+          get users_path
+          assert_select "title", full_title("All users")
+        assert_select "a[href=?]", user_path(user)
+          get user_path(user)
+          assert_select "title", full_title(user.name)
+        assert_select "a[href=?]", edit_user_path(user)
+          get edit_user_path(user)
+          assert_select "title", full_title("Edit user")
   end
 end
