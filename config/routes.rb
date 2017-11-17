b2794f25de32fe2d6ba0b04e7da659ef97ce5de2
Josh_Gerth <jgerth@ggc.edu>
2017-11-17 07:57:10 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  
   root  'static_pages#home'
+  
   get   '/help',    to: 'static_pages#help'
   get   '/about',   to: 'static_pages#about'
   get   '/contact', to: 'static_pages#contact'
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   
   resources :users
+  resources :microposts,          only: [:create, :destroy]
 end
