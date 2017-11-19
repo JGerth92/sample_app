aabac49f55f501b2357bf297bf5fe2869ca05efd
Josh_Gerth <jgerth@ggc.edu>
2017-11-19 04:33:15 +0000

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   
   resources :users
+  resources :account_activations, only: [:edit]
   resources :microposts,          only: [:create, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,16 @@ User.create!(name:  "John Doe",
              email: "jdoe@example.com",
              password:              "123456",
              password_confirmation: "123456",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
 
 98.times do |n|
   name  = Faker::Name.name
@@ -23,7 +27,9 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
 
 users = User.order(:created_at).take(6)
